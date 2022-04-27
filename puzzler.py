0997b10cf7c43bfbafa71c5fb4c80667ad885a25
Belzedar94 <54615238+Belzedar94@users.noreply.github.com>
2022-04-27 10:31:55 +0200

    Update puzzler.py

--- puzzler.py
+++ puzzler.py
@@ -19,7 +19,7 @@ def has_cp(info_line, score=0):
     return info_line['score'][0] == 'cp' and int(info_line['score'][1]) > score
 
 
-def get_puzzle(variant, fen, moves, engine, depth):
+def get_puzzle(variant, fen, moves, engine, depth, evalfile):
     if len(sf.legal_moves(variant, fen, moves)) <= 2:
         return None, None
 
