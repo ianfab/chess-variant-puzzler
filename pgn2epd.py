6b183d7ddc01695f531364665044ce3b17196d3e
gbtami <gbtami@gmail.com>
2023-07-13 09:16:31 +0200

    Collect positions evaluated as mate in x by lichess

--- pgn2epd.py
+++ pgn2epd.py
@@ -17,15 +17,17 @@ def game_count(filename):
 
 
 class PrintAllFensVisitor(chess.pgn.BaseVisitor):
-    def __init__(self, variant=None):
+    def __init__(self, variant=None, mate=False):
         super(PrintAllFensVisitor, self).__init__()
         self.variant = variant
+        self.mate = mate
 
     def begin_game(self):
         self.uci_variant = ""
         self.fens = []
         self.relevant = True
         self.site = ""
+        self.board_eval = ""
 
     def visit_header(self, name, value):
         if name == "Variant":
@@ -41,8 +43,26 @@ class PrintAllFensVisitor(chess.pgn.BaseVisitor):
             # Optimization hint: Do not even bother parsing the moves.
             return chess.pgn.SKIP
 
+    def visit_comment(self, comment):
+        if comment.startswith("[%eval"):
+            self.board_eval = comment
+        else:
+            self.board_eval = None
+            self.relevant = False
+
+    def visit_move(self, board, move):
+        if self.relevant and self.mate:
+            ev = self.board_eval
+            if ev is None:
+                self.relevant = False
+            else:
+                if ev.startswith("[%eval #"):
+                    self.fens.append(
+                        "{};variant {};site {}".format(board.fen(), self.uci_variant, self.site)
+                    )
+
     def visit_board(self, board):
-        if self.relevant:
+        if self.relevant and not self.mate:
             # python-chess can't recognize 960 in non Chess960 games
             # but using it for non 960 games as well doesn't hurt
             board.chess960 = True
@@ -57,8 +77,8 @@ class PrintAllFensVisitor(chess.pgn.BaseVisitor):
         return self.fens
 
 
-def write_fens(pgn_file, stream, variant, count):
-    visitor = functools.partial(PrintAllFensVisitor, variant=variant)
+def write_fens(pgn_file, stream, variant, count, mate):
+    visitor = functools.partial(PrintAllFensVisitor, variant=variant, mate=mate)
     with open(pgn_file) as pgn:
         with tqdm(total=game_count(pgn_file)) as pbar:
             cnt = 0
@@ -67,7 +87,7 @@ def write_fens(pgn_file, stream, variant, count):
                 pbar.update(1)
                 if fens is None:
                     break
-                elif len(fens) == 0:
+                elif len(fens) == 0 and not mate:
                     continue
                 else:
                     cnt += 1
@@ -84,6 +104,7 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--input-file", help="pgn file containing lichess games")
     parser.add_argument("-v", "--variant", help="variant to generate positions for")
     parser.add_argument("-c", "--count", type=int, default=1000, help="number of games")
+    parser.add_argument("-m", "--mate", action="store_true", help="only mate positions")
 
     args = parser.parse_args()
-    write_fens(args.input_file, sys.stdout, args.variant, args.count)
+    write_fens(args.input_file, sys.stdout, args.variant, args.count, args.mate)
